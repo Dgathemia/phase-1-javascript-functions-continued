638fafe94c6513bc69051c429c30aef733164f43
danielgathemia <danielgathemia01@gmail.com>
2022-08-25 23:08:41 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+
+function mondayWork(activity = 'go to the office') {
+    return(`This Monday, I will ${activity}.`)
+}
+mondayWork("work from home")
+
+
+function wrapAdjective(wrap = '*'){
+    return function(wrap2 = "a hard worker"){
+        let emphatic = `You are ${wrap}${wrap2}${wrap}!`
+        return emphatic
+    }
+}
+wrapAdjective()()
